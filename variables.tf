60e6b3d7120039f96b8f425679ca622d78f50a85
malij9 <jhojo.malicdem@bhp.com>
2022-07-21 13:43:37 +0800

    remove region

--- variables.tf
+++ variables.tf
@@ -35,6 +35,29 @@ variable "sftp_user_map" {
   }
 }
 
+#account details
+/* variable "region" {
+  type        = string
+  description = "default aws region"
+} */
+
+variable "access_key" {
+  type        = string
+  description = "aws access key for aws provider"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "aws secret key for aws provider"
+}
+
+/* variable "aws_tags" {
+  description = "Tagging for ownership and cost information."
+  type        = map(any)
+  default = null
+
+} */
+
 
 #learn work terraform workspaces here - https://www.terraform.io/docs/state/workspaces.html
 # command to initilize environment specific workspace ( dev, qa, prd)
@@ -44,14 +67,14 @@ variable "sftp_user_map" {
 # terraform workspace new "dev"
 # terraform plan
 # terraform apply
-locals {
+/* locals {
   workspace_env = "${lookup(var.workspace_to_environment_map, terraform.workspace, "dev")}"
   #convert to uppercase 
   environment      = "${upper(local.workspace_env)}"
   sftp_bucket_name = "${var.sftp_bucket_name_map[local.workspace_env]}"
   sftp_user        = "${var.sftp_user_map[local.workspace_env]}"
 
-}
+} */
 
 
 variable "owner_name" {
