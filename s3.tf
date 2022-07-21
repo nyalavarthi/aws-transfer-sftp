f88c8e1b9eb7f929ad29bff7bf87fcbefe18e5b1
malij9 <jhojo.malicdem@bhp.com>
2022-07-21 14:13:20 +0800

    fix warning

--- s3.tf
+++ s3.tf
@@ -10,7 +10,7 @@ resource "aws_kms_key" "mykey" {
 
 resource "aws_s3_bucket" "b" {
   bucket = "sftp-bucket-ny1"
-  acl    = "private"
+
   #region = "us-east-1"
   versioning {
     enabled = true
@@ -21,6 +21,7 @@ resource "aws_s3_bucket" "b" {
     Owner       = var.owner_name,
     DataType    = "SFTP files"
   }
+
 
   /* 
   logging {
@@ -41,7 +42,18 @@ resource "aws_s3_bucket" "b" {
   }
 
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "b_encryption" {
+    bucket = aws_s3_bucket.b.bucket
 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+}
+resource "aws_s3_bucket_acl" "b_bucket_acl" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.b.id
 
