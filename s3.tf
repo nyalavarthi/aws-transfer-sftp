d82851ed8eded66b504f894931396cce56861469
malij9 <jhojo.malicdem@bhp.com>
2022-07-21 14:14:49 +0800

    fix warning

--- s3.tf
+++ s3.tf
@@ -49,6 +49,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "b_encryption" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
+  }
 }
 resource "aws_s3_bucket_acl" "b_bucket_acl" {
   bucket = aws_s3_bucket.b.id
